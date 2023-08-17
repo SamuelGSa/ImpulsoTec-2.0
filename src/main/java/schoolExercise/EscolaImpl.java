f972c1a67de98aeaa88809dacd09b73521e533b4
SamuelGSa <sgmsa@icloud.com>
2023-08-17 12:07:32 -0300

    isolando metodos

---
+++ src/main/java/schoolExercise/EscolaImpl.java
@@ -0,0 +1,79 @@
+package schoolExercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EscolaImpl {
+
+    static void mediaDaNotaPorAno(Escola escola) {
+
+        System.out.println("=== ___ ===___===");
+        System.out.println("MEDIA DE NOTAS DA TURMA");
+        System.out.println("Primeiro Ano: A:" + mediaNotaTurma(escola.getPrimeiroAno().getTurmaA().getAlunos()) +
+                " B:" + mediaNotaTurma(escola.getPrimeiroAno().getTurmaB().getAlunos()) +
+                " C:" + mediaNotaTurma(escola.getPrimeiroAno().getTurmaC().getAlunos()));
+
+        System.out.println("Segundo Ano: A:" + mediaNotaTurma(escola.getSegundoAno().getTurmaA().getAlunos()) +
+                " B:" + mediaNotaTurma(escola.getSegundoAno().getTurmaB().getAlunos()) +
+                " C:" + mediaNotaTurma(escola.getSegundoAno().getTurmaC().getAlunos()));
+
+        System.out.println("Terceiro Ano: A:" + mediaNotaTurma(escola.getTerceiroAno().getTurmaA().getAlunos()) +
+                " B:" + mediaNotaTurma(escola.getTerceiroAno().getTurmaB().getAlunos()) +
+                " C:" + mediaNotaTurma(escola.getTerceiroAno().getTurmaC().getAlunos()));
+    }
+
+    static void porcentagemAprovadosPorAno(Escola escola) {
+
+        System.out.println("=== ___ ===___===");
+        System.out.println("PORCENTAGEM DE ALUNOS APROVADOS: ");
+        System.out.println("Primeiro Ano: A:" + porcentagemAprovados(escola.getPrimeiroAno().getTurmaA().getAlunos()) +
+                " B:" + porcentagemAprovados(escola.getPrimeiroAno().getTurmaB().getAlunos()) +
+                " C:" + porcentagemAprovados(escola.getPrimeiroAno().getTurmaC().getAlunos()));
+
+        System.out.println("Segundo Ano: A:" + porcentagemAprovados(escola.getSegundoAno().getTurmaA().getAlunos()) +
+                " B:" + porcentagemAprovados(escola.getSegundoAno().getTurmaB().getAlunos()) +
+                " C:" + porcentagemAprovados(escola.getSegundoAno().getTurmaC().getAlunos()));
+
+        System.out.println("Terceiro Ano: A:" + porcentagemAprovados(escola.getTerceiroAno().getTurmaA().getAlunos()) +
+                " B:" + porcentagemAprovados(escola.getTerceiroAno().getTurmaB().getAlunos()) +
+                " C:" + porcentagemAprovados(escola.getTerceiroAno().getTurmaC().getAlunos()));
+    }
+
+    private static void listaAlunoNota(List<Alunos> alunosTurma) {
+        alunosTurma.stream().forEach(S -> {
+            System.out.println("Nome: " + S.getNome() + " Nota: " + S.getNota());
+        });
+    }
+
+    private static Integer mediaNotaTurma(List<Alunos> alunosTurma) {
+
+        List<Integer> notas = new ArrayList<>();
+        alunosTurma.stream().forEach(S -> {
+            notas.add(S.getNota());
+        });
+
+        int sum = notas.stream().mapToInt(Integer::intValue).sum();
+
+        return sum / notas.size();
+
+    }
+
+
+    private static String porcentagemAprovados(List<Alunos> alunosTurma) {
+
+        var aprovados = filtraAlunosAprovados(alunosTurma).size();
+        var porcentagem = (aprovados * 100) / alunosTurma.size();
+
+        return String.valueOf(porcentagem).concat("%");
+    }
+
+    private static List<String> filtraAlunosAprovados(List<Alunos> alunosTurma) {
+
+        List<String> alunosAprovados = new ArrayList<>();
+        alunosTurma.stream().forEach(Alunos -> { if (Alunos.getNota() >= 3){
+            alunosAprovados.add("Nome: " + Alunos.getNome() + " = " + "Nota: " +  Alunos.getNota());
+        }
+        });
+        return alunosAprovados;
+    }
+}
--- src/main/java/schoolExercise/SistemaEscolar.java
+++ src/main/java/schoolExercise/SistemaEscolar.java
@@ -3,6 +3,9 @@ package schoolExercise;
 import java.util.ArrayList;
 import java.util.List;
 
+import static schoolExercise.EscolaImpl.mediaDaNotaPorAno;
+import static schoolExercise.EscolaImpl.porcentagemAprovadosPorAno;
+
 public class SistemaEscolar {
 
     /**
@@ -26,79 +29,5 @@ public class SistemaEscolar {
         porcentagemAprovadosPorAno(escola);
         mediaDaNotaPorAno(escola);
 
-
-
-    }
-
-    private static void mediaDaNotaPorAno(Escola escola) {
-
-        System.out.println("=== ___ ===___===");
-        System.out.println("MEDIA DE NOTAS DA TURMA");
-        System.out.println("Primeiro Ano: A:" + mediaNotaTurma(escola.getPrimeiroAno().getTurmaA().getAlunos()) +
-                " B:" + mediaNotaTurma(escola.getPrimeiroAno().getTurmaB().getAlunos()) +
-                " C:" + mediaNotaTurma(escola.getPrimeiroAno().getTurmaC().getAlunos()));
-
-        System.out.println("Segundo Ano: A:" + mediaNotaTurma(escola.getSegundoAno().getTurmaA().getAlunos()) +
-                " B:" + mediaNotaTurma(escola.getSegundoAno().getTurmaB().getAlunos()) +
-                " C:" + mediaNotaTurma(escola.getSegundoAno().getTurmaC().getAlunos()));
-
-        System.out.println("Terceiro Ano: A:" + mediaNotaTurma(escola.getTerceiroAno().getTurmaA().getAlunos()) +
-                " B:" + mediaNotaTurma(escola.getTerceiroAno().getTurmaB().getAlunos()) +
-                " C:" + mediaNotaTurma(escola.getTerceiroAno().getTurmaC().getAlunos()));
-    }
-
-    private static void porcentagemAprovadosPorAno(Escola escola) {
-
-        System.out.println("=== ___ ===___===");
-        System.out.println("PORCENTAGEM DE ALUNOS APROVADOS: ");
-        System.out.println("Primeiro Ano: A:" + porcentagemAprovados(escola.getPrimeiroAno().getTurmaA().getAlunos()) +
-                " B:" + porcentagemAprovados(escola.getPrimeiroAno().getTurmaB().getAlunos()) +
-                " C:" + porcentagemAprovados(escola.getPrimeiroAno().getTurmaC().getAlunos()));
-
-        System.out.println("Segundo Ano: A:" + porcentagemAprovados(escola.getSegundoAno().getTurmaA().getAlunos()) +
-                " B:" + porcentagemAprovados(escola.getSegundoAno().getTurmaB().getAlunos()) +
-                " C:" + porcentagemAprovados(escola.getSegundoAno().getTurmaC().getAlunos()));
-
-        System.out.println("Terceiro Ano: A:" + porcentagemAprovados(escola.getTerceiroAno().getTurmaA().getAlunos()) +
-                " B:" + porcentagemAprovados(escola.getTerceiroAno().getTurmaB().getAlunos()) +
-                " C:" + porcentagemAprovados(escola.getTerceiroAno().getTurmaC().getAlunos()));
-    }
-
-    private static void listaAlunoNota(List<Alunos> alunosTurma) {
-        alunosTurma.stream().forEach(S -> {
-                    System.out.println("Nome: " + S.getNome() + " Nota: " + S.getNota());
-                });
-    }
-
-    private static Integer mediaNotaTurma(List<Alunos> alunosTurma) {
-
-        List<Integer> notas = new ArrayList<>();
-        alunosTurma.stream().forEach(S -> {
-            notas.add(S.getNota());
-        });
-
-        int sum = notas.stream().mapToInt(Integer::intValue).sum();
-
-        return sum / notas.size();
-
-    }
-
-
-    private static String porcentagemAprovados(List<Alunos> alunosTurma) {
-
-        var aprovados = filtraAlunosAprovados(alunosTurma).size();
-        var porcentagem = (aprovados * 100) / alunosTurma.size();
-
-        return String.valueOf(porcentagem).concat("%");
-    }
-
-    private static List<String> filtraAlunosAprovados(List<Alunos> alunosTurma) {
-
-        List<String> alunosAprovados = new ArrayList<>();
-        alunosTurma.stream().forEach(Alunos -> { if (Alunos.getNota() >= 3){
-            alunosAprovados.add("Nome: " + Alunos.getNome() + " = " + "Nota: " +  Alunos.getNota());
-        }
-        });
-        return alunosAprovados;
     }
 }
